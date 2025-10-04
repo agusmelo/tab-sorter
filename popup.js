8984edfa07d87f009eb3505f9b60f4dcfdade8f0
Agustin <amagustin.melo@gmail.com>
2025-10-04 01:02:08 -0300

    wip garbage collection

--- popup.js
+++ popup.js
@@ -5,7 +5,10 @@ document.getElementById('countBtn').addEventListener('click', async () => {
 
     //hacer cosas con chrome.tabs.Tab.lastAccessed?: number | undefined;
     const tabs = await chrome.tabs.query({currentWindow: true});
-    const tab = tabs[0];
+    if(areTabsSorted(tabs)){
+        status.textContent = 'Tabs are already sorted';
+        return;
+    }
     const groupedTabs = Object.groupBy(tabs,(tab) => tab.url ? getDomainAndTLD(tab.url) : "others")
     
     const sortedKeys = Object.keys(groupedTabs).sort()
@@ -18,22 +21,49 @@ document.getElementById('countBtn').addEventListener('click', async () => {
         // o tambien puedo splittear si tengo mil tabsa en una sola window
         await chrome.tabs.move(groupedTabs[key].map((tab)=> tab.id),{index:-1}); 
     }
-    console.log({groupedTabs})
+    // console.log({groupedTabs})
 
 
 
     status.textContent = tab ? tab.url : 'No active tab';
   } catch (err) {
+    console.log(err);
     status.textContent = 'Error: ' + err.message;
   }
 });
 
 
+document.getElementById('garbageBtn').addEventListener('click', async () => {
+  const tabs = await chrome.tabs.query({currentWindow: true});
+  const garbageTabIds = []
+  tabs.forEach((tab)=>{
+    if(tab.lastAccessed && (tab.lastAccessed - Date.now()) > 36000000 && tab.id ){
+      console.log({lastAccessed: tab.lastAccessed})
+      garbageTabIds.push(tab.id)
+    }
+  })
+  chrome.tabs.group({tabIds: garbageTabIds})
+  console.log({garbageTabIds})
+});
+
+
 function getDomainAndTLD(url) {
+  if (!url) return "zzz.zzz";
   const hostname = new URL(url).hostname;
   const parts = hostname.split(".");
   if (parts.length >= 2) {
     return parts.slice(-2).join(".");
   }
   return hostname;
+}
+
+function areTabsSorted(tabs) {
+  for (let i = 1; i < tabs.length; i++) {
+    const prevDomain = getDomainAndTLD(tabs[i - 1].url || "");
+    const currDomain = getDomainAndTLD(tabs[i].url || "");
+    if (prevDomain > currDomain) {
+      return false;
+    } 
+  }
+  return true;
 }
