1a78b5d0e6d7def0d05ffc47ee01d49db05b74b5
Agustin Melo <amagustinmelo8@gmail.com>
2026-05-24 16:23:22 -0300

    garbage collection

--- popup.js
+++ popup.js
@@ -1,5 +1,10 @@
 import { sortTabs } from "./actions.js";
 
+let GARBAGE_GROUP_ID = null;
+const GARBAGE_GROUP_TITLE = "Basura";
+const THREE_HOURS_IN_MS = 3 * 60 * 60 * 1000;
+
+
 document.getElementById('countBtn').addEventListener('click', async ()=>{
   const status = document.getElementById('status');
   status.textContent = 'Fetching active tab...'; 
@@ -11,13 +16,25 @@ document.getElementById('garbageBtn').addEventListener('click', async () => {
   const tabs = await chrome.tabs.query({currentWindow: true});
   const garbageTabIds = []
   tabs.forEach((tab)=>{
-    if(tab.lastAccessed && (tab.lastAccessed - Date.now()) > 36000000 && tab.id ){
+    if(tab.lastAccessed && (Date.now() - tab.lastAccessed ) > THREE_HOURS_IN_MS && tab.id ){
       console.log({lastAccessed: tab.lastAccessed})
       garbageTabIds.push(tab.id)
+      console.log(`Tab ${tab.id} is garbage - tab.lastAccessed: ${tab.lastAccessed} - now: ${Date.now()}`)
     }
   })
-  chrome.tabs.group({tabIds: garbageTabIds})
-  console.log({garbageTabIds})
+  console.log(`Garbage tab IDs: ${garbageTabIds.join(', ')}`);
+  if (garbageTabIds.length === 0) {
+    console.log('No garbage tabs found');
+    return;
+  }
+  if(GARBAGE_GROUP_ID === null){
+    const groupId = await chrome.tabs.group({tabIds: garbageTabIds});
+    GARBAGE_GROUP_ID = groupId;
+    await chrome.tabGroups.update(GARBAGE_GROUP_ID, {title: GARBAGE_GROUP_TITLE, color: 'grey'});
+  } else {
+    await chrome.tabs.group({groupId: GARBAGE_GROUP_ID ,tabIds: garbageTabIds})
+  }
+  console.log({GARBAGE_GROUP_ID})
 });
 
 
